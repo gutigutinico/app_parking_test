6ad0332588c5efff3bab26f7ff96a1f61779e486
Sebastian G <orjuela.desarrollador@gmail.com>
2025-03-07 13:04:21 -0500

    url

--- js/config_global.js
+++ js/config_global.js
@@ -1,2 +1,2 @@
 const API_URL_SCHEMA = "https://apppar-test.iglesiadulcerefugio.org/api/v1/{SERVICE}/gBm4YRrg5am2FxgLhKw4P3euzf5fdR";
-const ROOT_DIR = "http://127.0.0.1:5050/";
+const ROOT_DIR = "https://gutigutinico.github.io/app_parking_test/";
