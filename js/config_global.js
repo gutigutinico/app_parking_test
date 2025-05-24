c31ff454460b86e476f0ce7bf274f1d73480f713
Nicolas Gutierrez Orjuela <gutigutinico@gmail.com>
2025-05-24 15:02:46 -0500

    ajuste

--- js/config_global.js
+++ js/config_global.js
@@ -1,2 +1,2 @@
-const API_URL_SCHEMA = "http://192.168.20.220:8000/api/v1/{SERVICE}/gBm4YRrg5am2FxgLhKw4P3euzf5fdR";
-const ROOT_DIR = "http://192.168.20.220:4200/";
+const API_URL_SCHEMA = "https://apppar-test.iglesiadulcerefugio.org/api/v1/{SERVICE}/gBm4YRrg5am2FxgLhKw4P3euzf5fdR";
+const ROOT_DIR = "https://gutigutinico.github.io/app_parking_test/";
